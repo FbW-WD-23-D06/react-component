1337b895875a94ef672acc567902bb6a3259aaec
giacomotolari <tolarigiacomo@gmail.com>
2023-12-05 10:33:11 +0100

    create ImageContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import "./App.css";
+import ImageContainer from "./components/ImageContainer";
+
 
 function App() {
   return (
     <>
       <h1 className="text-align-center">Beaches</h1>
-      <div className="beach-container">
-        <h6>Beach name - beach location</h6>
-        <img
-          src="https://images.unsplash.com/photo-1519046904884-53103b34b206?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJlYWNofGVufDB8fDB8fHww
-            "
-          alt="beach"
-        />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, eius!
-          Dolorum dolorem placeat nulla! Itaque, labore sapiente aliquid
-          assumenda sed, quas nulla fuga recusandae quo voluptas asperiores
-          ullam odio! Iusto.
-        </p>
-      </div>
+      <ImageContainer />
     </>
   );
 }
 
 export default App;
+
---
+++ src/components/ImageContainer.jsx
@@ -0,0 +1,17 @@
+export default function ImageContainer() {
+  return (
+    <div className="beach-container">
+      <h6>Beach name - beach location</h6>
+      <img
+        src="https://images.unsplash.com/photo-1519046904884-53103b34b206?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJlYWNofGVufDB8fDB8fHww"
+        alt="beach"
+      />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, eius!
+        Dolorum dolorem placeat nulla! Itaque, labore sapiente aliquid assumenda
+        sed, quas nulla fuga recusandae quo voluptas asperiores ullam odio!
+        Iusto.
+      </p>
+    </div>
+  );
+}
