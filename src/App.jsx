31d6178033ccfba29f173ccb68d9c946de5d9d39
giacomotolari <tolarigiacomo@gmail.com>
2023-12-05 13:19:38 +0100

    key addedd and render all ImageContainer with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import "./App.css";
 import ImageContainer from "./components/ImageContainer";
+import beaches from "./data/beaches.json";
 
 function App() {
-  const beachName = "Marina di Pescoluse ";
   return (
     <>
       <h1 className="text-align-center">Beaches</h1>
-      <ImageContainer
-        classValue="beach-container" // in JSX if we have to write a "string" we don't need to use {}
-        title={`${beachName} - Salento`} // if using template literals we need to use {}
-        imageSrc="https://images.unsplash.com/photo-1559494007-9f5847c49d94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGJlYWNofGVufDB8fDB8fHww"
-        description="lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, eius! Dolorum dolorem placeat nulla! Itaque, labore sapiente aliquid assumenda sed, quas nulla fuga recusandae quo voluptas asperiores ullam odio! Iusto."
-        annualVistors={2000000}
-      />
-
-      <ImageContainer
-        classValue="beach-container"
-        title="Maya Bay - Thailand"
-        imageSrc="https://images.unsplash.com/photo-1519046904884-53103b34b206?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJlYWNofGVufDB8fDB8fHww"
-        description="lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, eius! Dolorum dolorem placeat nulla! Itaque, labore sapiente aliquid assumenda sed, quas nulla fuga recusandae quo voluptas asperiores ullam odio! Iusto."
-        annualVistors={123324000}
-      />
-
-      <ImageContainer
-        classValue="beach-container"
-        title="Fiji - Oceania"
-        imageSrc="https://images.unsplash.com/photo-1519046904884-53103b34b206?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJlYWNofGVufDB8fDB8fHww"
-        description="lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, eius! Dolorum dolorem placeat nulla! Itaque, labore sapiente aliquid assumenda sed, quas nulla fuga recusandae quo voluptas asperiores ullam odio! Iusto."
-        annualVistors={6664000}
-      />
+      {beaches.map((beach) => {
+        return (
+          <ImageContainer
+            key={crypto.randomUUID()}
+            {...beach}
+            // title={beach.title}
+            // imageSrc={beach.imageSrc}
+            // description={beach.description}
+            // classValue="beach-container"
+            // annualVistors={beach.annualVistors}
+          />
+        );
+      })}
     </>
   );
 }
