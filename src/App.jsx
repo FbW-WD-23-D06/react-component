4df1acaea09d90cf439de57085cc7ba307ba9265
giacomotolari <tolarigiacomo@gmail.com>
2023-12-05 15:16:46 +0100

    unused code deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import "./App.css";
-import ImageContainer from "./components/ImageContainer";
-import beaches from "./data/beaches.json";
+
 
 function App() {
   return (
     <>
-      <h1 className="text-align-center">Beaches</h1>
-      {beaches.map((beach) => {
-        return (
-          <ImageContainer
-            key={crypto.randomUUID()}
-            {...beach}
-            // title={beach.title}
-            // imageSrc={beach.imageSrc}
-            // description={beach.description}
-            // classValue="beach-container"
-            // annualVistors={beach.annualVistors}
-          />
-        );
-      })}
+     
     </>
   );
 }
